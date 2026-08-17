06880385284621c1315b6418d49cb236d1ab5138
Rajdeep Shukla <rajdeepshukla690@gmail.com>
2026-08-17 11:12:09 +0530

    Update SQLpractical1.sql

--- SQLpractical1.sql
+++ SQLpractical1.sql
@@ -35,3 +35,13 @@ mysql> CREATE TABLE enrollment (
     -> FOREIGN KEY (roll_no) REFERENCES student(roll_no),
     -> FOREIGN KEY (course_id) REFERENCES course(course_id)
     -> );
+
+INSERT INTO department Values(1, 'Computer Science'), (2, 'Electronics');
+
+INSERT INTO student Values(101, 'Nilisha', 'nilisha@mail.com', '123456789012', 1 );
+INSERT INTO student Values(102, 'Rahul', 'rahul@mail.com', '987654321098', 2 );
+
+INSERT INTO course Values(501, 'DBMS', 1 ),(502, 'Circuits', 2);
+
+INSERT INTO enrollment Values (101, 501, 3, 'A');
+INSERT INTO enrollment Values (101, 502, 3, 'B'); -- same student, different course; allowed
