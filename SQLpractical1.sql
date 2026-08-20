cda370ec808c103304bf41ecc31c6218ce687bbc
Rajdeep Shukla <rajdeepshukla690@gmail.com>
2026-08-20 10:23:21 +0530

    Update SQLpractical1.sql

--- SQLpractical1.sql
+++ SQLpractical1.sql
@@ -1,52 +1,49 @@
-mysql> CREATE DATABASE college_demo;
-Query OK, 1 row affected (0.03 sec)
+CREATE DATABASE college_demo;
 
-mysql> USE college_demo;
+USE college_demo;
 Database changed
-mysql> CREATE TABLE department(
-    -> dept_id INT PRIMARY KEY,
-    -> dept_name VARCHAR(50) UNIQUE NOT NULL
-    -> );
-Query OK, 0 rows affected (0.17 sec)
+    
+CREATE TABLE department(
+     dept_id INT PRIMARY KEY,
+     dept_name VARCHAR(50) UNIQUE NOT NULL
+     );
 
-mysql> CREATE TABLE student (
-    -> roll_no INT PRIMARY KEY,
-    -> name VARCHAR(50) NOT NULL,
-    -> email VARCHAR(50) UNIQUE,
-    -> dept_id INT,
-    -> FOREIGN KEY (dept_id) REFERENCES department(dept_id)
-    -> );
-Query OK, 0 rows affected (0.06 sec)
+CREATE TABLE student (
+     roll_no INT PRIMARY KEY,
+     name VARCHAR(50) NOT NULL,
+     email VARCHAR(50) UNIQUE,
+     dept_id INT,
+     FOREIGN KEY (dept_id) REFERENCES department(dept_id)
+     );
 
-mysql> CREATE TABLE course (
-    -> course_id INT PRIMARY KEY,
-    -> course_name VARCHAR(50) NOT NULL,
-    -> dept_id INT,
-    -> FOREIGN KEY (dept_id) REFERENCES department(dept_id)
-    -> );
-Query OK, 0 rows affected (0.04 sec)
+CREATE TABLE course (
+     course_id INT PRIMARY KEY,
+     course_name VARCHAR(50) NOT NULL,
+     dept_id INT,
+     FOREIGN KEY (dept_id) REFERENCES department(dept_id)
+     );
 
-mysql> CREATE TABLE enrollment (
-    -> roll_no INT,
-    -> course_id INT,
-    -> semester INT CHECK (semester BETWEEN 1 and 8),
-    -> grade CHAR(2),
-    -> PRIMARY KEY (roll_no, course_id, semester),
-    -> FOREIGN KEY (roll_no) REFERENCES student(roll_no),
-    -> FOREIGN KEY (course_id) REFERENCES course(course_id)
-    -> );
+CREATE TABLE enrollment (
+     roll_no INT,
+     course_id INT,
+     semester INT CHECK (semester BETWEEN 1 and 8),
+     grade CHAR(2),
+     PRIMARY KEY (roll_no, course_id, semester),
+     FOREIGN KEY (roll_no) REFERENCES student(roll_no),
+     FOREIGN KEY (course_id) REFERENCES course(course_id)
+     );
 
-mysql> INSERT INTO department Values(1, 'Computer Science'), (2, 'Electronics');
+INSERT INTO department Values(1, 'Computer Science'), (2, 'Electronics');
 
-     ->  INSERT INTO student Values(101, 'Nilisha', 'nilisha@mail.com', '123456789012', 1 );
-     ->  INSERT INTO student Values(102, 'Rahul', 'rahul@mail.com', '987654321098', 2 );
+       INSERT INTO student Values(101, 'Nilisha', 'nilisha@mail.com', '123456789012', 1 );
+       INSERT INTO student Values(102, 'Rahul', 'rahul@mail.com', '987654321098', 2 );
 
-     -> INSERT INTO course Values(501, 'DBMS', 1 ),(502, 'Circuits', 2);
+       INSERT INTO course Values(501, 'DBMS', 1 ),(502, 'Circuits', 2);
 
-     ->  INSERT INTO enrollment Values (101, 501, 3, 'A');
-     ->  INSERT INTO enrollment Values (101, 502, 3, 'B'); -- same student, different course; allowed
+       INSERT INTO enrollment Values (101, 501, 3, 'A');
+       INSERT INTO enrollment Values (101, 502, 3, 'B'); -- same student, different course; allowed
 
-mysql> SELECT * FROM department;
-     ->  SELECT * FROM student;
-     ->  SELECT * FROM course;
-     ->  SELECT * FROM enrollment;
+SELECT * FROM department;
+       SELECT * FROM student;
+       SELECT * FROM course;
+       SELECT * FROM enrollment;
